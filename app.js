c4fcc95272e2cf73fb8ab4de11cf1cf1193403b0
Roy Ali Hasan <royalihasan0987@gmail.com>
2023-01-29 17:07:56 +0500

    home end point added

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 app.use("/v1/api", require("./routes/history.routes"));
 
+app.get("/", (req, res) => {
+  res.send({ message: "Hello I'm Your Friend BitGet History Fetcher" });
+});
+
 const server = () => {
   try {
     app.listen(process.env.PORT || 3000, () =>
