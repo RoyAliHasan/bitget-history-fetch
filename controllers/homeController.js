8f7db3befdff09168a32c8168cec133f4ec73a4c
Roy Ali Hasan <royalihasan0987@gmail.com>
2023-01-30 14:01:09 +0500

    get Req Added in index.ejs

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,5 +1,22 @@
+const { SpotClient } = require("bitget-api");
 const homeController = (req, res) => {
-  res.render("index", { name: "Ali Hasan" });
+  const client = new SpotClient({
+    apiKey: process.env.API_KEY,
+    apiSecret: process.env.SECRETE_KEY,
+    apiPass: process.env.PASS_PHRASE,
+  });
+  client
+    .getApiKeyInfo()
+    .then((result) => {
+      console.log("GET: ApiKEY INFO:", result);
+      res.render("index", { result: result });
+    })
+
+    .catch((err) => {
+      console.error("ERROR: API KEY : ", err);
+    });
+
+  console.log("LOGGING: HOME PAGE ");
 };
 
 module.exports = { homeController };
