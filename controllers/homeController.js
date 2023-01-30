886bf0040e5ab0554d55a1cd746252a99b3c0396
Roy Ali Hasan <royalihasan0987@gmail.com>
2023-01-31 00:22:09 +0500

    Home page Edited

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,26 +1,5 @@
-const { SpotClient } = require("bitget-api");
 const homeController = (req, res) => {
-  const client = new SpotClient({
-    apiKey: process.env.API_KEY,
-    apiSecret: process.env.SECRETE_KEY,
-    apiPass: process.env.PASS_PHRASE,
-  });
-  client
-    .getOrderHistory("IXTUSDT_SPBL", {
-      before: 122222,
-      after: 222222,
-      limit: 10,
-    })
-    .then((result) => {
-      console.log("GET: ApiKEY INFO:", result);
-      res.render("index", { result: result });
-    })
-
-    .catch((err) => {
-      console.error("ERROR: API KEY : ", err);
-    });
-
-  console.log("LOGGING: HOME PAGE ");
+  res.render("index");
 };
 
 module.exports = { homeController };
